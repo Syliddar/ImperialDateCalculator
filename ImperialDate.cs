fb226faef6eb9c2d4462f72c0597c2f745175596
Jason Myers <jmyers@varsity.com>
2019-07-10 11:04:07 -0500

    added explanatory comments

--- ImperialDate.cs
+++ ImperialDate.cs
@@ -28,6 +28,7 @@ namespace ImperialDateConversion
 
         private static string GetYear(DateTime sourceDate)
         {
+            //The Millenium portion of the year is tracked separately.
             var year = sourceDate.Year.ToString();
             return year.Substring(1);
         }
@@ -41,6 +42,16 @@ namespace ImperialDateConversion
 
     public enum CheckNumber
     {
+        /*
+         * The "Check Number" of a date is a measurement of how accurate to Earth time the event took place in.
+         * The Imperium of Man uses faster-than-light psychic communication across the warp, and all dates are syncronized to Earth time.
+         *      0 - An event that took place *on* Earth
+         *      1 - Somewhere in the Sol system
+         *      2 - In direct communication with Earth
+         *      3 - In direct communication with someone that is also in direct communication with Earth
+         * ...and so on and so forth. Each additional step away from Earth is an additional step in the Check Number. 
+         * The numbers 6-8 are used when not in warp communication with anyone, but when warp communication with a class 0-5 was made within the indicated timeframe.
+         */
         Terra = 0,
         SolSystem = 1,
         Direct = 2,
